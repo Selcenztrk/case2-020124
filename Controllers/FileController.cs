c96da6dd64c1cf10a46af33871d87c03d902dec3
Selcen Öztürk <selcenztrk@gmail.com>
2024-02-01 22:06:37 +0300

    filter v2
    Now filter can handle *.apk, Hu*.pdf like requests.

--- Controllers/FileController.cs
+++ Controllers/FileController.cs
@@ -31,15 +31,23 @@ namespace case2_020124.Controllers
                 List<string> fileNames = Directory.GetFileSystemEntries(filePath)
                                                            .Select(Path.GetFileName)
                                                            .ToList();
+                if (keyword.Contains("*"))
+                {
+                    fileNames = fileNames.Where(p => p.StartsWith(keyword.Split('*')[0]) && Path.GetExtension(p) == keyword.Split('*')[1]).ToList();
+                }
+                else
+                {
+                    fileNames = fileNames.Where(file => string.IsNullOrEmpty(keyword) || Path.GetExtension(file) == keyword).ToList();
 
-                fileNames = fileNames.Where(file => string.IsNullOrEmpty(keyword) || Path.GetExtension(file) == keyword).ToList();
-
+                }
                 return Ok(new { FileNames = fileNames });
             }
             catch (Exception ex)
             {
                 return StatusCode(500, new { errorMessage = ex.Message });
             }
+
+
         }
     }
 }
