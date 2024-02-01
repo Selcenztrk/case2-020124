c4a4dab3b3d06b516b110aead0cbb960b820713a
Selcen Öztürk <selcenztrk@gmail.com>
2024-02-01 21:30:39 +0300

    getFileNames API

---
+++ Controllers/FileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace case2_020124.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        [HttpGet("fileNames")]
+        public IActionResult GetFileNames(string filePath)
+        {
+            try
+            {
+                List<string> fileNames = Directory.GetFileSystemEntries(filePath)
+                                                  .Select(Path.GetFileName)
+                                                  .ToList();
+
+                return Ok(new { FileNames = fileNames });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { errorMessage = ex.Message });
+            }
+        }
+    }
+}
